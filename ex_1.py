04ac550bc0015c3f7340e61ed407ff986e1fce72
Emanuel <emnarlanferreira@gmail.com>
2024-11-30 14:14:45 -0300

    first commit

---
+++ ex_1.py
@@ -0,0 +1,5 @@
+nome = 'Emanuel'
+idade = '21'
+cidade = 'Camaçari'
+
+print(f'Sou {nome} tenho {idade} anos e moro em {cidade}')
---
+++ ex_2.py
@@ -0,0 +1,6 @@
+num1 = 5
+num2 = 7
+
+soma = num1 + num2
+
+print(f'{num1} + {num2} = {soma}')
